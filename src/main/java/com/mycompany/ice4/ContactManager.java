68390e9838c7fef16fba9ccbd8c59683881220e6
pollocje <jeffrey.pollock123@gmail.com>
2024-11-19 20:05:05 -0500

    Revert ""
    This reverts commit 95944ac853fcdd53dd3ba191546c2efa08714547.

--- src/main/java/com/mycompany/ice4/ContactManager.java
+++ src/main/java/com/mycompany/ice4/ContactManager.java
@@ -2,9 +2,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
-
-// this is a comment i am adding because of GIT and how it STINKS
 package com.mycompany.ice4;
 
 /**
